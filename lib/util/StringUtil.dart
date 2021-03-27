336804efb2f8f9e202f3f9766c9df34fe1def473
Yodi Satriani <yodisatriani@live.com>
2021-03-27 22:22:35 +0800

    add formatter for duration in StringUtil

--- lib/util/StringUtil.dart
+++ lib/util/StringUtil.dart
@@ -7,4 +7,27 @@ class StringUtil {
 
     return parsedString;
   }
+
+  // d in second
+  static String formatDurationHHmm(int durationInSecond) {
+    if (durationInSecond == null || durationInSecond == 0) return "0 sec";
+    String result = "";
+    int hour = durationInSecond ~/ 3600;
+    int minute = durationInSecond % 3600 ~/ 60;
+    int second = durationInSecond % 3600 % 60;
+
+    if (hour > 0) {
+      result += hour.toString() + " ";
+      result += (hour > 1) ? "hours " : "hour ";
+    }
+    if (minute > 0) {
+      result += minute.toString() + " ";
+      result += (minute > 1) ? "mins " : "min ";
+    }
+    if (second > 0) {
+      result += second.toString() + " ";
+      result += (second > 1) ? "secs " : "sec";
+    }
+    return result;
+  }
 }
