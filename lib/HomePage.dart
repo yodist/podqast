b1586fbbf1e071bcafaf930c3669a6e63cc30308
Yodi Satriani <yodisatriani@live.com>
2021-04-04 13:05:27 +0800

    tidy up podcastHome

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/controller/PodcastController.dart';
-import 'package:flutter_application_1/widget/podcastBlock.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_application_1/widget/podcastHome.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key, required this.title}) : super(key: key);
@@ -68,52 +67,4 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-}
-
-Widget podcastHome(Future<Map<String, dynamic>> data) {
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.start,
-    children: <Widget>[
-      Container(
-        margin: EdgeInsets.fromLTRB(15, 10, 0, 10),
-        child: Text(
-          "Top Podcasts",
-          style: GoogleFonts.robotoCondensed(
-              fontSize: 28, fontWeight: FontWeight.bold),
-        ),
-        alignment: Alignment.centerLeft,
-      ),
-      Container(
-        alignment: Alignment.topLeft,
-        height: 250.0,
-        child: ListView(
-          scrollDirection: Axis.horizontal,
-          children: <Widget>[
-            FutureBuilder(
-                future: data,
-                builder: (context, snapshot) {
-                  return snapshot.hasData
-                      ? ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          shrinkWrap: true,
-                          itemCount: (snapshot.data! as Map)['podcasts'].length,
-                          itemBuilder: (_, int position) {
-                            var snapshotData = (snapshot.data! as Map);
-                            final item = snapshotData['podcasts'][position];
-                            return podcastBlock(context, item['id'],
-                                item['image'], item['title'],
-                                publisher: item['publisher'],
-                                description: item['description'],
-                                genreIds: item['genreIds']);
-                          },
-                        )
-                      : Center(
-                          child: CircularProgressIndicator(),
-                        );
-                }),
-          ],
-        ),
-      ),
-    ],
-  );
 }
